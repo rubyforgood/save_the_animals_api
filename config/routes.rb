e67c282e23ef51a3651a00327ed08e6fde72b374
Russell Osborne <russell@theironyard.com>
2017-05-16 10:18:09 -0400

    Adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
   namespace :api do
     post :auth, controller: :auth, action: :create
   end
+  root to: "home#index"
 end
