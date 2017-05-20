9e0bcc6ec18a08422725dc4e1d9bba88bfa68986
Russell Osborne <russell@burningpony.com>
2017-05-20 14:20:14 -0400

    Nest ethograms under data key

--- app/controllers/api/ethograms_controller.rb
+++ app/controllers/api/ethograms_controller.rb
@@ -3,10 +3,10 @@
 module Api
   class EthogramsController < BaseController
     def index
-      render json: Ethogram.all.as_json(
+      render json: { data: Ethogram.all.as_json(
         only: %i[id name],
         methods: %i[subjects behaviors structure_id]
-      )
+      ) }
     end
   end
 end
