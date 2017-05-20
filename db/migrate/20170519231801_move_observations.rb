45551c5df684a3c835edb775333d16a5eb847860
Russell Osborne <russell@burningpony.com>
2017-05-20 12:32:12 -0400

    Fixed foreign key to be uuid for observation_session

--- db/migrate/20170519231801_move_observations.rb
+++ db/migrate/20170519231801_move_observations.rb
@@ -2,7 +2,7 @@ class MoveObservations < ActiveRecord::Migration[5.1]
   def change
     remove_column :observation_sessions, :observations
     create_table :observations, id: :uuid do |t|
-      t.integer :observation_session_id
+      t.uuid :observation_session_id
       t.jsonb :details
       t.timestamps
     end
