5bedb004796c7132e890b25bd74c5ced770be31b
Marc Bellingrath <marrrc.b@gmail.com>
2017-05-20 20:24:15 -0400

    Eliminating gaps in our code.

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -6,10 +6,10 @@ class Observation < ApplicationRecord
     observation_session_id 'observation_session_id'
     observation_session :user_id => 'user_id'
     observation_session 'user_email' do |os| os.user&.email end
-    details ('subject') { |d| d['subject'] }
-    details ('modifier') { |d| d['modifier'] }
-    details ('behavior') { |d| d['behavior'] }
-    details ('target') { |d| d['target'] }
+    details('subject') { |d| d['subject'] }
+    details('modifier') { |d| d['modifier'] }
+    details('behavior') { |d| d['behavior'] }
+    details('target') { |d| d['target'] }
     created_at 'uploaded_at'
   end
 end
