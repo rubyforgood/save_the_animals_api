0198680f2361788bd64cf70e8bbf892a71414a2e
Andy Lindeman <andy@lindeman.io>
2017-05-21 11:11:46 -0400

    Sets up SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,14 @@ Rails.application.configure do
     host: "save-the-animals.herokuapp.com"
   }
 
+  config.action_mailer.smtp_settings = {
+    address:   ENV.fetch("SMTP_ADDRESS", "smtp.gmail.com"),
+    port:      ENV.fetch("SMTP_PORT", "465").to_i,
+    user_name: ENV["SMTP_USERNAME"],
+    password:  ENV["SMTP_PASSWORD"],
+    tls:       true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
