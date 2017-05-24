5127c65642494d5bf4e4607df945e72e768086ec
Will Murphy <willmurphyscode@gmail.com>
2017-05-23 21:36:53 -0400

    removes n+1 query. clean up of code coming soon

--- app/controllers/api/observation_sessions_controller.rb
+++ app/controllers/api/observation_sessions_controller.rb
@@ -4,13 +4,33 @@ module Api
       ActiveRecord::Base.transaction do
         create_observation_sessions
         # TODO: upsert?
-        observations = observations_params.map do |observation|
+        puts observations_params.inspect
+        # observations = observations_params.map do |observation|
+        #   observation_session = ObservationSession.find_by(id: observation['observation_session_id'])
+        #   Observation.find_or_create_by(id: observation['id']) do |record|
+        #     record.details = observation
+        #     record.observation_session = observation_session
+        #     record.save
+        #   end
+        # end
+
+        observations = []
+        session_ids_to_instances =  observations_params
+          .map {|param| param['observation_session_id']}
+          .uniq
+          .map { |session_id| ObservationSession.find_or_create_by(id: session_id) }
+          .group_by { |session| session.id }
+        
+ 
+        observations_params.each do | observation |
           Observation.find_or_create_by(id: observation['id']) do |record|
             record.details = observation
-            record.observation_session_id = observation['observation_session_id']
+            record.observation_session = session_ids_to_instances[observation['observation_session_id']][0]
+            record.save
+            observations << record
           end
         end
-
+        
         response = observations.map { |o| { id: o.id, persisted: o.persisted?, errors: o.errors.full_messages } }
         render json: response, status: :ok
       end
@@ -20,13 +40,26 @@ module Api
 
     private
 
+    # def create_observation_sessions
+    #   observations_params
+    #     .map { |o| o['observation_session_id'] }
+    #     .uniq
+    #     .map { |id| { id: id, user_id: current_user.id } }
+    #     .map do |attrs|
+    #     ObservationSession.create_once(attrs)
+    #   end
+    # end
+
     def create_observation_sessions
-      observations_params
+      unique_sessions = observations_params
         .map { |o| o['observation_session_id'] }
         .uniq
+
+      selector_map = unique_sessions
         .map { |id| { id: id, user_id: current_user.id } }
-        .map do |attrs|
-        ObservationSession.create_once(attrs)
+      
+      selector_map.each do |selector|
+        ObservationSession.create_once(selector)
       end
     end
 
