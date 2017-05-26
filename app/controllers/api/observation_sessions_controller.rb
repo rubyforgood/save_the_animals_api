932cae74e414f6c95aaab44ecf886fc3418c687f
Will Murphy <willmurphyscode@gmail.com>
2017-05-26 08:10:12 -0400

    Aesthetic Refactor
    Remove the redundate `save` call from the block passed to
    `find_or_create_by` and change `each` to `map` to avoid an unnecessary
    array.

--- app/controllers/api/observation_sessions_controller.rb
+++ app/controllers/api/observation_sessions_controller.rb
@@ -1,24 +1,22 @@
+# frozen_string_literal: true
+
 module Api
   class ObservationSessionsController < BaseController
     def create
       ActiveRecord::Base.transaction do
-        
-      session_ids_to_instances = {} 
-      processed_observations = []   
+        session_ids_to_instances = {}
 
-      observations_params.each do | observation |
-          session = session_ids_to_instances.fetch(observation['observation_session_id']) do | session_id |
+        processed_observations = observations_params.map do |observation|
+          session = session_ids_to_instances.fetch(observation['observation_session_id']) do |session_id|
             session_ids_to_instances[session_id] = ObservationSession.find_or_create_by(id: session_id, user_id: current_user.id)
           end
-          
+
           Observation.find_or_create_by(id: observation['id']) do |record|
             record.details = observation
             record.observation_session = session
-            record.save
-            processed_observations << record
           end
         end
-        
+
         response = processed_observations.map { |o| { id: o.id, persisted: o.persisted?, errors: o.errors.full_messages } }
         render json: response, status: :ok
       end
